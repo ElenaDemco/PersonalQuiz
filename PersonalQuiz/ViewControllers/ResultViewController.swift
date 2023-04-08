f56321928145be2c3f3281a7a92027b9fbd26ce1
Elena <elena.demco@gmail.com>
2023-04-08 20:42:26 +0200

    Rename array and add methods

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,16 +9,41 @@ import UIKit
 
 final class ResultViewController: UIViewController {
     
+    @IBOutlet var resultTitle: UILabel!
+    
+    @IBOutlet var resultDefinition: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.hidesBackButton.toggle()
+        navigationItem.hidesBackButton = true
     }
     
-    var dataAnswers: [Answer] = []
-    
+    var answers: [Answer]!
+    var animalDefinition = Animal.cat
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
     
+    private func calculatedAnswers(from answers: [Answer]) -> Animal? {
+        let animalCounts = answers.reduce(into: [:]) { counts, answer in
+            counts[answer.animal, default: 0] += 1
+        }
+        
+        let frequentAnimals = animalCounts.filter { $0.value == animalCounts.values.max() }.keys
+        return frequentAnimals.first
+    }
+    func getResultTitle() -> String {
+        if let animal = answers.first?.animal {
+            resultTitle.text = "Вы - \(String(animal.rawValue))"
+        }
+        return ""
+    }
+    
+    func getResultDefinition() -> String {
+        if let animal = answers.first?.animal {
+            resultDefinition.text = animal.definition
+        }
+        return ""
+    }
 }
